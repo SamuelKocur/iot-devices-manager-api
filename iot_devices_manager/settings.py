9a766620fb421c3a8921db9c2b088ffa280ddd5e
SamuelKocur <samuel.kocur@vacuumlabs.com>
2023-02-17 13:06:28 +0100

    Add logout and logout-all endpoints

--- iot_devices_manager/settings.py
+++ iot_devices_manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-xckeq$rcm%==chm)7n2cmne*e+41b3odism*0k-q((**y-ul=y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from knox import views as knox_views
+
 
 from . import views
 
@@ -6,4 +8,7 @@ urlpatterns = [
     path('login', views.LoginApiView.as_view(), name="login"),
     path('register', views.RegisterApiView.as_view(), name="register"),
     path('change-password', views.ChangePasswordApiView.as_view(), name="change-password"),
+
+    path('logout/', knox_views.LogoutView.as_view()),
+    path('logout-all/', knox_views.LogoutAllView.as_view()),
 ]

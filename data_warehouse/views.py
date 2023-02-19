ee1f60122dfe2164e62538da83e7967b78e10323
SamuelKocur <samuel.kocur@vacuumlabs.com>
2023-02-19 19:57:41 +0100

    Add empty serializer to FilterDataListView

--- data_warehouse/views.py
+++ data_warehouse/views.py
@@ -2,11 +2,12 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView, get_object_or_404
 
 from data_warehouse.cron import add_data_to_warehouse
+from iot_devices_manager.utils.serializers import EmptySerializer
 
 
 class FilterDataListView(GenericAPIView):
+    serializer_class = EmptySerializer
+
     def get(self, request):
         add_data_to_warehouse()
         return Response()
-
-

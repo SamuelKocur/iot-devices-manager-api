495d688ef03a16ff7309e3a480c9e4fcf2445b17
SamuelKocur <samuel.kocur@vacuumlabs.com>
2023-03-14 18:56:32 +0100

    Add home page and download logic

--- iot_devices_manager/view.py
+++ iot_devices_manager/view.py
@@ -1,7 +1,19 @@
+import os
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index_view(request):
-    response = render(request, 'index.html')
+    file_path = 'media/mobile/android/app-arm64-v8a-release.apk'
+    last_modified = os.path.getmtime(file_path)
+    last_modified_date_android = datetime.datetime.fromtimestamp(last_modified)
+    last_modified_date_ios = ""
+    context = {
+        'last_modified_android': last_modified_date_android,
+        'last_modified_ios': last_modified_date_ios,
+    }
+
+    response = render(request, 'index.html', context)
     return HttpResponse(response)
